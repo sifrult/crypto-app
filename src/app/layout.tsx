5b0824208a7c7a1b5253732a6f8758587fb26c10
snarra <amy.snarr16@gmail.com>
2024-05-29 10:59:48 -0600

    added basic links to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -17,9 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div style={{height: '100px', background: 'red'}}></div>
-        
-        {children}</body>
+        <div style={{ height: "100px", background: "red" }}>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Link href="/">Home</Link>
+            <Link href="/Portfolio">Portfolio</Link>
+          </div>
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
